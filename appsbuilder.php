919505a31ecd605efa3ab839250fef4ecec3d23e
AppsBuilder <AppsBuilder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-06 14:30:02 +0000

    modifiche gigi
    git-svn-id: https://plugins.svn.wordpress.org/native-apps-builder/trunk@405487 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- appsbuilder.php
+++ appsbuilder.php
@@ -4,7 +4,7 @@
     Plugin URI: http://www.apps-builder.com/ 
     Description: With Native Apps Builder Plugin you can create 100% <strong>NATIVE app version</strong> of your wordpress'site. Available native apps for iPhone iPad Android and Tablets. In 6 easy steps you'll able to download native apps and submitting them in the <strong>Apple Store</strong> and <strong>Market Android</strong>. Native apps builder now support webapp and native apps.
     Author: Apps Builder srl 
-    Version: 1.0 Beta 
+    Version: 2.0 Beta 
     Author URI: http://www.apps-builder.com 
     */  
 add_action('admin_menu', 'my_plugin_menu');
